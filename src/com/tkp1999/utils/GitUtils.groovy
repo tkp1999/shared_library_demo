2abfef99871c1730e568ef24cb714c003198577a
Tarun Kumar Pal <tarunkumarpal468@gmail.com>
2025-01-11 16:09:39 +0530

    added changes

--- src/com/tkp1999/utils/GitUtils.groovy
+++ src/com/tkp1999/utils/GitUtils.groovy
@@ -1,23 +1,24 @@
+
 package com.tkp1999.utils
 
 class GitUtils {
+    def script // To store the pipeline script context
+
+    // Constructor to accept the pipeline script context
+    GitUtils(script) {
+        this.script = script
+    }
+
     def checkoutRepo(Map args) {
         def repoUrl = args.url
         def branch = args.branch ?: 'main'
 
-        println "Checking out repository: ${repoUrl} on branch: ${branch}"
-        checkout([$class: 'GitSCM',
-                  branches: [[name: "*/${branch}"]],
-                  userRemoteConfigs: [[url: repoUrl]]
+        script.println "Checking out repository: ${repoUrl} on branch: ${branch}"
+        script.checkout([$class: 'GitSCM',
+                         branches: [[name: "*/${branch}"]],
+                         userRemoteConfigs: [[url: repoUrl]]
         ])
     }
-/*
-    def createAndPushTag(String repoUrl, String buildNumber) {
-        def tagName = "build-${buildNumber}"
-        println "Creating tag: ${tagName}"
-        sh "git tag ${tagName}"
-        sh "git push ${repoUrl} ${tagName}"
-    }
-    */
 }
+
 
--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,27 +1,21 @@
 import com.tkp1999.utils.*
+
+import com.tkp1999.utils.GitUtils
+
 def call(Map config) {
     node(config.nodeLabel) {
         stage('Checkout') {
             script {
-                def gitUtils = new com.tkp1999.utils.GitUtils()
+                // Pass the pipeline script context (this) to GitUtils
+                def gitUtils = new GitUtils(this)
                 gitUtils.checkoutRepo(
                     url: config.repoUrl,
                     branch: config.branch
                 )
             }
         }
-        /*
-        stage('Build') {
-            script {
-                sh "mvn clean install"
-            }
-        }
-        stage('Tag and Push') {
-            script {
-                def gitUtils = new com.company.utils.GitUtils()
-                gitUtils.createAndPushTag(config.repoUrl, config.buildNumber)
-            }
-        }
-        */
     }
 }
+
+
+
