fec6d3df7f73971aa6062155e7a95492233931c5
Tarun Kumar Pal <tarunkumarpal468@gmail.com>
2025-01-11 15:31:00 +0530

    added fixes

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -2,7 +2,7 @@ def call(Map config) {
     node(config.nodeLabel) {
         stage('Checkout') {
             script {
-                def gitUtils = com.tkp1999.utils.GitUtils()
+                def gitUtils = new com.tkp1999.utils.GitUtils()
                 gitUtils.checkoutRepo(
                     url: config.repoUrl,
                     branch: config.branch
