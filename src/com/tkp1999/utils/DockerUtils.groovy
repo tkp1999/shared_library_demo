f9eb2205ac146c4f8646f1bf1f883a0946d7dcca
Tarun Kumar Pal <tarunkumarpal468@gmail.com>
2025-01-28 16:17:20 +0530

    fixed docker login issue

--- src/com/tkp1999/utils/DockerUtils.groovy
+++ src/com/tkp1999/utils/DockerUtils.groovy
@@ -47,8 +47,8 @@ class DockerUtils {
                 echo "Logging in to Docker registry..."
                 //echo \$DOCKER_PASS | docker login ${registryUrl} -u \$DOCKER_USER --password-stdin
                 //echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin
-                //docker login -u \$DOCKER_USER -p \$DOCKER_PASS
-                docker login -u '$DOCKER_USER' -p '$DOCKER_PASS'
+                docker login -u \$DOCKER_USER -p \$DOCKER_PASS
+                //docker login -u '$DOCKER_USER' -p '$DOCKER_PASS'
                 script.echo "login succeeded"
                 //docker build -t ${registryUrl}/${imageName}:${tag} .
                 docker build -t ${DOCKER_USER}/${imageName}:${tag} .
