8e207fb84ebada776cca95722a3f0328db343972
tkp1999 <37674937+tkp1999@users.noreply.github.com>
2025-04-02 12:16:42 +0530

    Update trivy_image_scan.groovy for multiple report types

--- src/com/tkp1999/utils/trivy_image_scan.groovy
+++ src/com/tkp1999/utils/trivy_image_scan.groovy
@@ -1,6 +1,7 @@
 package com.tkp1999.utils
 
 class trivy_image_scan {
+    /*
     def script
 
     trivy_image_scan(script) {
@@ -23,6 +24,34 @@ class trivy_image_scan {
             pwd
         """
         
+        script.sh "docker rmi ${trivyImage} || true"
+        script.sh "ls -l /tmp"
+    }
+    */
+    def script
+
+    trivy_image_scan(script) {
+        this.script = script
+    }
+
+    def scanDockerImage(String trivyImage, String targetImage, List<String> reportFormats) {
+        script.sh "docker pull ${trivyImage}"
+
+        def validFormats = ["json", "html", "spdx", "csv", "table", "template", "sarif", "cyclonedx", "spdx-json", "github", "cosign-vuln"]
+        
+        for (format in reportFormats) {
+            format = format.trim()
+            if (!validFormats.contains(format)) {
+                script.error "Invalid format: ${format}. Supported formats: ${validFormats.join(', ')}"
+            }
+
+            def outputFile = "/reports/report.${format}"
+            script.sh """
+                docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/reports ${trivyImage} image --format ${format} --output ${outputFile} ${targetImage}
+                echo "Scan completed. Report saved as ${outputFile}."
+            """
+        }
+
         script.sh "docker rmi ${trivyImage} || true"
         script.sh "ls -l /tmp"
     }
